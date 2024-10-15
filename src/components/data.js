5fc11fa495a84a534513c7985493aed215b8585c
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-10-16 06:06:48 +0800

    feat: create a file data to be a container for default cv values

---
+++ src/components/data.js
@@ -0,0 +1,53 @@
+const generalInfoData = {
+  firstName: "David",
+  LastName: "Gibbons",
+  Mobile: "079 3316 8146",
+  Email: "David.gibbons@live.co.uk",
+};
+
+const educationalExperience = [
+  {
+    year: "2009-2013",
+    school: "Aston University",
+    nameOfStudy: "BSe Computer Science",
+    titleOfStudy: "1st Class Degree with Professional Placement",
+  },
+  {
+    year: "2007-2009",
+    school: "Hall Green College",
+    nameOfStudy: "BTEC National Diploman in IT",
+    titleOfStudy: "Grade Achieved: Triple Distinction",
+  },
+  {
+    year: "2000-2007",
+    school: "Bournville Secondary School",
+    titleOfStudy: "8 GCSEs at grades A*-C",
+  },
+];
+
+const practicalExperience = [
+  {
+    companyName: "Maplins",
+    positionTitle: "IT Manager",
+    date: "Jun 2008 - Present",
+    mainResponsibility: [
+      "Mentoring and training new IT staff",
+      "Researching, installing and configuring new computer systems",
+      "Ensuring that all relevant licensing laws are adhered to",
+      "Keeping up to date with the latest technologies",
+    ],
+  },
+  {
+    companyName: "Ladypool Warehouse Ltd.",
+    positionTitle: "IT Support Officer",
+    date: "Oct 2003 - Jun 2008",
+    mainResponsibility: [
+      "Provided extensive IT support to internal and external stakeholders",
+      "Installed and configured hardware operating systems and applications",
+      "Monitored and maintained computer systems and networks",
+      "Resolved, diagnosed and solved network",
+    ],
+  },
+];
+
+export { generalInfoData, educationalExperience, practicalExperience };
