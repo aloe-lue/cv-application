e6fd2b09fe4cc928084b9b1ef30934332842b2d2
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-10-21 11:13:25 +0800

    feat: create a container for forms and cv
    show form components by default
    show cv by clicking a button that change the value of isCvShow
    
    render the general info with general info data
    don't render practical experience component if practical experience data
    length is zero do it so same with education experience component
    
    render cv forms div and each section containing form.
    each section receives data and a handler via prop to render each
    form inputs value and replace state var to this App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,86 @@
-const App = () => {};
+import GeneralInfo from "./components/general-info.jsx";
+import EducationalExperience from "./components/educational-experience.jsx";
+import PracticalExperience from "./components/practical-experience.jsx";
+import {
+  GenInfoSection,
+  EducExpSection,
+  PractExpSection,
+} from "./components/display.jsx";
+import { useState } from "react";
+import {
+  educationalExperience,
+  generalInfoData,
+  practicalExperience,
+} from "./components/data.js";
+import "./style/style.css";
+
+const App = () => {
+  const [genInfo, setGenInfo] = useState(generalInfoData);
+  const [educExp, setEducExp] = useState(educationalExperience);
+  const [practExp, setPractExp] = useState(practicalExperience);
+  const [isCvShow, setIsCvShow] = useState(false);
+
+  const { firstName, LastName, Mobile, Email, location } = genInfo;
+
+  const handleShowCv = () => {
+    setIsCvShow(!isCvShow);
+  };
+  const handleGenInfo = (bunch) => {
+    setGenInfo(bunch);
+  };
+
+  return (
+    <>
+      <header>
+        <h1>CV Application</h1>
+        <button onClick={() => handleShowCv()}>
+          {isCvShow ? "CV Form" : "Display Cv"}
+        </button>
+      </header>
+      <main>
+        <div className="main-content">
+          {!isCvShow && (
+            <div className="cv-form">
+              <GeneralInfo
+                genInfo={genInfo}
+                handler={handleGenInfo}
+              ></GeneralInfo>
+
+              <EducationalExperience
+                educExpArray={educExp}
+                handler={setEducExp}
+              ></EducationalExperience>
+
+              <PracticalExperience
+                practExpArray={practExp}
+                handler={setPractExp}
+              ></PracticalExperience>
+            </div>
+          )}
+          {isCvShow && (
+            <div className="CV">
+              <div className="cv-output">
+                <GenInfoSection
+                  firstName={firstName}
+                  LastName={LastName}
+                  Mobile={Mobile}
+                  Email={Email}
+                  location={location}
+                ></GenInfoSection>
+
+                {educExp.length > 0 && (
+                  <EducExpSection array={educExp}></EducExpSection>
+                )}
+                {practExp.length > 0 && (
+                  <PractExpSection array={practExp}></PractExpSection>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default App;
